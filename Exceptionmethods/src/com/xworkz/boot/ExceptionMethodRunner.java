565b7d8a355b240453cb5d180d801e5830a3b64c
aishzz <aishwaryark.xworkz@gmail.com>
2022-12-07 09:24:49 +0530

    methods of eception

---
+++ Exceptionmethods/src/com/xworkz/boot/ExceptionMethodRunner.java
@@ -0,0 +1,111 @@
+package com.xworkz.boot;
+
+import com.xworkz.custom.ExceptionMethod;
+
+public class ExceptionMethodRunner {
+
+	public static void main(String[] args) {
+		ExceptionMethod.method1();
+		ExceptionMethod.method2();
+		ExceptionMethod.method3();
+		ExceptionMethod.method4();
+		ExceptionMethod.method5();
+		ExceptionMethod.method6();
+		ExceptionMethod.method7();
+		ExceptionMethod.method8();
+		ExceptionMethod.method9();
+		ExceptionMethod.method10();
+		ExceptionMethod.method11();
+		ExceptionMethod.method12();
+		ExceptionMethod.method13();
+		ExceptionMethod.method14();
+		ExceptionMethod.method15();
+		ExceptionMethod.method16();
+		ExceptionMethod.method17();
+		ExceptionMethod.method18();
+		ExceptionMethod.method19();
+		ExceptionMethod.method20();
+		ExceptionMethod.method21();
+		ExceptionMethod.method22();
+		ExceptionMethod.method23();
+		ExceptionMethod.method24();
+		ExceptionMethod.method26();
+		ExceptionMethod.method27();
+		ExceptionMethod.method28();
+		ExceptionMethod.method29();
+		ExceptionMethod.method30();
+		ExceptionMethod.method31();
+		ExceptionMethod.method32();
+		ExceptionMethod.method33();
+		ExceptionMethod.method34();
+		ExceptionMethod.method35();
+		ExceptionMethod.method36();
+		ExceptionMethod.method37();
+		ExceptionMethod.method38();
+		ExceptionMethod.method39();
+		ExceptionMethod.method40();
+		ExceptionMethod.method41();
+		ExceptionMethod.method42();
+		ExceptionMethod.method43();
+		ExceptionMethod.method44();
+		ExceptionMethod.method45();
+		ExceptionMethod.method46();
+		ExceptionMethod.method47();
+		ExceptionMethod.method48();
+		ExceptionMethod.method49();
+		ExceptionMethod.method50();
+		ExceptionMethod.method52();
+		ExceptionMethod.method53();
+		ExceptionMethod.method54();
+		ExceptionMethod.method55();
+		ExceptionMethod.method56();
+		ExceptionMethod.method57();
+		ExceptionMethod.method59();
+		ExceptionMethod.method60();
+		ExceptionMethod.method61();
+		ExceptionMethod.method62();
+		ExceptionMethod.method63();
+		ExceptionMethod.method64();
+		ExceptionMethod.method65();
+		ExceptionMethod.method66();
+		ExceptionMethod.method67();
+		ExceptionMethod.method68();
+		ExceptionMethod.method69();
+		ExceptionMethod.method70();
+		ExceptionMethod.method71();
+		ExceptionMethod.method72();
+		ExceptionMethod.method73();
+		ExceptionMethod.method74();
+		ExceptionMethod.method75();
+		ExceptionMethod.method76();
+		ExceptionMethod.method77();
+		ExceptionMethod.method78();
+		ExceptionMethod.method79();
+		ExceptionMethod.method80();
+		ExceptionMethod.method81();
+		ExceptionMethod.method82();
+		ExceptionMethod.method83();
+		ExceptionMethod.method84();
+		ExceptionMethod.method85();
+		ExceptionMethod.method86();
+		ExceptionMethod.method87();
+		ExceptionMethod.method88();
+		ExceptionMethod.method89();
+		ExceptionMethod.method90();
+		ExceptionMethod.method91();
+		ExceptionMethod.method92();
+		ExceptionMethod.method93();
+		ExceptionMethod.method94();
+		ExceptionMethod.method95();
+		ExceptionMethod.method96();
+		ExceptionMethod.method97();
+		ExceptionMethod.method98();
+		ExceptionMethod.method99();
+		ExceptionMethod.method100();
+		
+		
+		
+		
+	}
+
+}
--- Exceptionmethods/src/com/xworkz/custom/ExceptionMethod.java
+++ Exceptionmethods/src/com/xworkz/custom/ExceptionMethod.java
@@ -4,7 +4,9 @@ import java.awt.AWTError;
 import java.awt.color.CMMException;
 import java.awt.datatransfer.MimeTypeParseException;
 import java.awt.image.ImagingOpException;
+import java.awt.print.PrinterException;
 import java.io.IOError;
+import java.io.IOException;
 import java.lang.annotation.AnnotationFormatError;
 import java.lang.annotation.AnnotationTypeMismatchException;
 import java.lang.invoke.WrongMethodTypeException;
@@ -16,6 +18,8 @@ import java.nio.charset.CoderMalfunctionError;
 import java.nio.charset.MalformedInputException;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.FileSystemAlreadyExistsException;
+import java.nio.file.FileSystemNotFoundException;
+import java.rmi.MarshalException;
 import java.rmi.NotBoundException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.NoSuchAlgorithmException;
@@ -31,11 +35,16 @@ import java.util.zip.DataFormatException;
 import javax.activity.InvalidActivityException;
 import javax.lang.model.UnknownEntityException;
 import javax.management.BadBinaryOpValueExpException;
+import javax.management.JMException;
 import javax.management.JMRuntimeException;
 import javax.naming.CannotProceedException;
 import javax.naming.NamingException;
+import javax.print.PrintException;
 import javax.print.attribute.UnmodifiableSetException;
 import javax.security.auth.RefreshFailedException;
+import javax.sound.sampled.LineUnavailableException;
+import javax.xml.bind.JAXBException;
+import javax.xml.crypto.KeySelectorException;
 import javax.xml.parsers.FactoryConfigurationError;
 import javax.xml.soap.SOAPException;
 import javax.xml.transform.TransformerException;
@@ -45,265 +54,461 @@ import org.ietf.jgss.GSSException;
 import org.omg.CORBA.UserException;
 import org.omg.CORBA.portable.RemarshalException;
 
+import com.sun.corba.se.impl.orbutil.threadpool.TimeoutException;
+import com.sun.javafx.geom.IllegalPathStateException;
 import com.sun.media.sound.InvalidDataException;
+import com.sun.org.apache.xerces.internal.impl.xpath.XPathException;
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.ParseException;
 import com.sun.org.apache.xml.internal.security.signature.XMLSignatureException;
 import com.sun.xml.internal.ws.policy.privateutil.ServiceConfigurationError;
 
 import netscape.javascript.JSException;
 
 public class ExceptionMethod {
-    public void method1() {
+    public static void method1() {
 	System.out.println("running cricket");
 	throw new RuntimeException();
     }
-	public void method2() throws ImagingOpException
+	public static void method2() throws ImagingOpException
 	{
 		System.out.println("running method running");
 		throw new ImagingOpException(null);
 	
 	}
- public void method3()throws ArithmeticException {
+ public static void method3()throws ArithmeticException {
 	 System.out.println("running printStack");
 	 throw new ArithmeticException();
  }
- public void method4()throws AnnotationTypeMismatchException {
+ public static void method4()throws AnnotationTypeMismatchException {
 	 System.out.println("running printStack");
 	 throw new ArithmeticException();
  }
- public void method5()throws ArrayStoreException {
+ public static void method5()throws ArrayStoreException {
 	 System.out.println("running printStack");
 	 throw new ArrayStoreException();
  }
- public void method6()throws BufferOverflowException {
+ public static void method6()throws BufferOverflowException {
 	 System.out.println("running printStack");
 	 throw new BufferOverflowException();
  }
- public void method7()throws BufferUnderflowException {
+ public  static void method7()throws BufferUnderflowException {
 	 System.out.println("running BufferUnderflowException");
 	 throw new BufferUnderflowException();
  }
- public void method8()throws MalformedInputException {
+ public static void method8()throws MalformedInputException {
 	 System.out.println("running MalformedInputException");
 	 throw new MalformedInputException(0);
  }
- public void method9()throws NoSuchAlgorithmException {
+ public static void method9()throws NoSuchAlgorithmException {
 	 System.out.println("running NoSuchAlgorithmException");
 	 throw new NoSuchAlgorithmException();
  }
- public void method10()throws ArithmeticException {
+ public static void method10()throws ArithmeticException {
 	 System.out.println("running printStack");
 	 throw new ArithmeticException();
  }
- public void method11()throws DataFormatException {
+ public static void method11()throws DataFormatException {
 	 System.out.println("running DataFormatException");
 	 throw new DataFormatException();
  }
- public void method12()throws MissingResourceException {
+ public static void method12()throws MissingResourceException {
 	 System.out.println("running MissingResourceException");
 	 throw new MissingResourceException(null, null, null);
  }
- public void method13()throws AnnotationFormatError{
+ public static void method13()throws AnnotationFormatError{
 	 System.out.println("running AnnotationFormatError");
 	 throw new AnnotationFormatError(null, null);
  }
- public void method14()throws AnnotationTypeMismatchException{
+ public static void method14()throws AnnotationTypeMismatchException{
 	 System.out.println("running AnnotationTypeMismatchException");
 	 throw new AnnotationTypeMismatchException(null, null);
  }
- public void method15()throws ImagingOpException{
+ public static void method15()throws ImagingOpException{
 	 System.out.println("running ImagingOpException");
 	 throw new ImagingOpException(null);
  }
- public void method16()throws FileSystemAlreadyExistsException{
+ public static void method16()throws FileSystemAlreadyExistsException{
 	 System.out.println("running FileSystemAlreadyExistsException");
 	 throw new FileSystemAlreadyExistsException(null);
  }	
- public void method17()throws FileSystemAlreadyExistsException, FileAlreadyExistsException{
+ public static void method17()throws FileSystemAlreadyExistsException, FileAlreadyExistsException{
 	 System.out.println("running FileSystemAlreadyExistsException");
 	 throw new FileAlreadyExistsException(null);
  }	
- public void method18()throws WrongMethodTypeException{
+ public static void method18()throws WrongMethodTypeException{
 	 System.out.println("running WrongMethodTypeException");
 	 throw new WrongMethodTypeException(null);
  }
- public void method19()throws EmptyStackException{
+ public static void method19()throws EmptyStackException{
 	 System.out.println("running EmptyStackException");
 	 throw new EmptyStackException();
  }
- public void method20()throws CMMException{
+ public static void method20()throws CMMException{
 	 System.out.println("running CMMException");
 	 throw new CMMException(null);
  }
- public void method21()throws UnsupportedAddressTypeException{
+ public static void method21()throws UnsupportedAddressTypeException{
 	 System.out.println("running UnsupportedAddressTypeException");
 	 throw new UnsupportedAddressTypeException();
  }
- public void method22()throws NoSuchAlgorithmException{
+ public static void method22()throws NoSuchAlgorithmException{
 	 System.out.println("running NoSuchAlgorithmException");
 	 throw new NoSuchAlgorithmException();
 	 
  }
- public void method23()throws ConcurrentModificationException{
+ public static void method23()throws ConcurrentModificationException{
 	 System.out.println("running ConcurrentModificationException");
 	 throw new ConcurrentModificationException();
 	 
  }
- public void method24()throws MissingResourceException{
+ public static void method24()throws MissingResourceException{
 	 System.out.println("running MissingResourceException");
 	 throw new MissingResourceException(null, null, null);
  }
- public void method25()throws CannotProceedException{
+ public static void method25()throws CannotProceedException{
 	 System.out.println("running CannotProcedeException");
 	 throw new CannotProceedException();
  }
- public void method26()throws BadBinaryOpValueExpException{
+ public static void method26()throws BadBinaryOpValueExpException{
 	 System.out.println("running BadBinaryOpValueExpException");
 	 throw new BadBinaryOpValueExpException(null);
  }
  
- public void method27()throws XMLSignatureException{
+ public static void method27()throws XMLSignatureException{
 	 System.out.println("running XMLSignatureException");
 	 throw new XMLSignatureException();
  }
- public void method28()throws BackingStoreException{
+ public  static void method28()throws BackingStoreException{
 	 System.out.println("running BackingStoreException");
 	 throw new BackingStoreException("hvhg");
  }
 
- public void method29()throws TransformerException,{
+ public static void method29()throws TransformerException{
 	 System.out.println("running TransformerException,");
 	 throw new TransformerException("vhsgu");
  }
- public void method30()throws AclNotFoundException{
+ public static void method30()throws AclNotFoundException{
 	 System.out.println("running AclNotFoundException");
 	 throw new AclNotFoundException();
  }
- public void method31()throws GSSException{
+ public static void method31()throws GSSException{
 	 System.out.println("running , GSSException");
 	 throw new GSSException(0);
  }
- public void method32()throws WrongMethodTypeException{
+ public static void method32()throws WrongMethodTypeException{
 	 System.out.println("running WrongMethodTypeException");
 	 throw new WrongMethodTypeException(null);
  }
- public void method33()throws NotOwnerException{
+ public static void method33()throws NotOwnerException{
 	 System.out.println("running NotOwnerException");
 	 throw new NotOwnerException();
  }
- public void method34()throws NamingException{
+ public static void method34()throws NamingException{
 	 System.out.println("running NamingException");
 	 throw new NamingException(null);
  }
- public void method35()throws RemarshalException{
+ public static void method35()throws RemarshalException{
 	 System.out.println("running RemarshalException");
 	 throw new RemarshalException();
  }
  
- public void method36() {
+ public static void method36() {
 	 System.out.println("AssertionError");
 	 throw new AssertionError();
  }
- public void method37() {
+ public static void method37() {
 	 System.out.println("AWTError,");
 	 throw new AWTError(null);
  }
- public void method38() {
+ public static void method38() {
 	 System.out.println("AnnotationFormatError,");
 	 throw new AnnotationFormatError("wehe");
  }
- public void method39() {
+ public static void method39() {
 	 System.out.println("ServiceConfigurationError,");
 	 throw new ServiceConfigurationError("nbdjd");
  }
- public void method40() {
+ public static void method40() {
 	 System.out.println("LinkageError,");
 	 throw new LinkageError(null);
  }
- public void method41() {
+ public static void method41() {
 	 System.out.println("CoderMalfunctionError,");
 	 throw new CoderMalfunctionError(null);
  }
- public void method42() {
+ public static void method42() {
 	 System.out.println("MimeTypeParseException,,");
 	 throw new MimeTypeParseException(null);
  }
- public void method43() {
+ public static void method43() {
 	 System.out.println("RefreshFailedException,");
 	 throw new RefreshFailedException(null);
  }
- public void method44() {
+ public static void method44() throws SOAPException {
 	 System.out.println("SOAPException,");
 	 throw new SOAPException();
  }
- public void method45() {
+ public static void method45() throws NotBoundException {
 	 System.out.println("NotBoundException,");
 	 throw new NotBoundException(null);
  }
- public void method46() {
+ public static void method46() {
 	 System.out.println("FactoryConfigurationError,");
 	 throw new FactoryConfigurationError();
  }
- public void method47() {
+ public static void method47() {
 	 System.out.println("TransformerFactoryConfigurationError,");
 	 throw new TransformerFactoryConfigurationError();
  }
- public void method48() {
+ public static void method48() {
 	 System.out.println("IOError,");
 	 throw new IOError(null);
  }
- public void method49() {
+ public static void method49() {
 	 System.out.println("TransformerFactoryConfigurationError,");
 	 throw new TransformerFactoryConfigurationError();
  }
- public void method50() {
+ public static void method50() {
 	 System.out.println("TransformerFactoryConfigurationError,");
 	 throw new TransformerFactoryConfigurationError();
  }
- public void method52() {
+ public static void method52() {
 	 System.out.println("ImagingOpException,");
 	 throw new ImagingOpException("bdhwguuh");
  }
- public void method53() {
+ public static void method53() {
 	 System.out.println("NegativeArraySizeException,");
 	 throw new NegativeArraySizeException("bdhwguuh");
  }
- public void method54() {
+ public static void method54() {
 	 System.out.println("JSException,");
 	 throw new JSException("bdhwguuh");
  }
- public void method55() {
+ public static void method55() {
 	 System.out.println(" JMRuntimeException,");
 	 throw new  JMRuntimeException("bdhwguuh");
  }
- public void method56() {
+ public static void method56() {
 	 System.out.println("UndeclaredThrowableException,");
 	 throw new UndeclaredThrowableException(null, "bdhwguuh");
  }
- public void method57() {
+ public static void method57() {
 	 System.out.println(" UnmodifiableModuleException,,");
 	 throw new  UnmodifiableSetException();
 	 
  }
- public void method58() {
+ public static void method58() {
 	 System.out.println(" InvalidAlgorithmParameterException,,");
 	 throw new  InvalidAlgorithmParameterException();
 	 
  }
- public void method59() {
+ public static void method59() {
 	 System.out.println(" InvalidActivityException,,");
 	 throw new  InvalidActivityException();
 	 
  }
- public void method60() {
+ public static void method60() {
 	 System.out.println(" InvalidDataException,,,");
 	 throw new  InvalidDataException();
 	 
  }
- public void metho61() {
+ public static void metho61() {
 	 System.out.println(" UnsupportedAddressTypeException,,");
 	 throw new  UnsupportedAddressTypeException();
 	 
  }
+ public void exceptionCretion62() throws IOException
+	{
+		System.out.println("running exceptionCretion20");
+		throw new IOException();
+	}
+	public void exceptionCretion63() throws JAXBException
+	{
+		System.out.println("running exceptionCretion21");
+		throw new JAXBException("jax");
+	}
+	public void exceptionCretion64() throws JMException
+	{
+		System.out.println("running exceptionCretion22");
+		throw new JMException();
+	}
+	public void exceptionCretion65()throws JMException
+	{
+		System.out.println("running exceptionCretion22");
+		throw new JMException();
+	}
+	public void exceptionCretion66() throws KeySelectorException
+	{
+		System.out.println("running exceptionCretion23");
+		throw new KeySelectorException();
+	}
+	public void exceptionCretion67() throws LastOwnerException
+	{
+		System.out.println("running exceptionCretion24");
+		throw new LastOwnerException();
+	}
+	public void exceptionCretion68() throws LineUnavailableException
+	{
+		System.out.println("running exceptionCretion25");
+		throw new LineUnavailableException();
+	}
+	public static void method69()throws BufferOverflowException {
+		 System.out.println("running printStack");
+		 throw new BufferOverflowException();
+	 }
+	 public  static void method70()throws BufferUnderflowException {
+		 System.out.println("running BufferUnderflowException");
+		 throw new BufferUnderflowException();
+	 }
+	 public void exceptionCretion71() throws MarshalException
+		{
+			System.out.println("running exceptionCretion26");
+			throw new MarshalException("marshal");
+		}
+		public void exceptionCretion72() throws MimeTypeParseException
+		{
+			System.out.println("running exceptionCretion27");
+			throw new MimeTypeParseException();
+		}
+		public void exceptionCretion73() throws NamingException
+		{
+			System.out.println("running exceptionCretion28");
+			throw new NamingException();
+		}
+		public void exceptionCretion74() throws NotBoundException
+		{
+			System.out.println("running exceptionCretion29");
+			throw new NotBoundException();
+		}
+		public void data75()
+		{
+			System.out.println("running data14");
+			throw new FileSystemNotFoundException();
+		}
+		public void data76()
+		{
+			System.out.println("running data15");
+			throw new IllegalArgumentException();
+		}
+		public void data77()
+		{
+			System.out.println("running data16");
+			throw new IllegalMonitorStateException();
+		}
+		public void data78()
+		{
+			System.out.println("running data17");
+			throw new IllegalPathStateException();
+		}
+		public void exceptionCretion79() throws ParseException
+		{
+			System.out.println("running exceptionCretion31");
+			throw new ProviderException();
+		}
+		public void exceptionCretion80() throws PrinterException
+		{
+			System.out.println("running exceptionCretion32");
+			throw new PrinterException();
+		}
+		public void exceptionCretion81() throws PrintException
+		{
+			System.out.println("running exceptionCretion33");
+			throw new PrintException();
+		}
+		public void exceptionCretion82() throws TimeoutException
+		{
+			System.out.println("running exceptionCretion34");
+			throw new TimeoutException();
+		}
+		public void exceptionCretion83() throws XPathException
+		{
+			System.out.println("running exceptionCretion35");
+			throw new XPathException("path");
+			
+		}
+		public void errorMethod84()
+		{
+			System.out.println("running errorMethod1");
+			throw new AnnotationFormatError("null");
+		}
+		public static void method85() {
+			 System.out.println("FactoryConfigurationError,");
+			 throw new FactoryConfigurationError();
+		 }
+		 public static void method86() {
+			 System.out.println("TransformerFactoryConfigurationError,");
+			 throw new TransformerFactoryConfigurationError();
+		 }
+		 public static void method87() {
+			 System.out.println("IOError,");
+			 throw new IOError(null);
+		 }
+		 public void errorMethod88()
+			{
+				System.out.println("running errorMethod");
+				throw new AssertionError();
+			}
+			public void errorMethod89()
+			{
+				System.out.println("running errorMethod");
+				throw new CoderMalfunctionError(null);
+			}
+			public void errorMethod90()
+			{
+				System.out.println("running errorMethod");
+				throw new FactoryConfigurationError();
+			}
+			public void errorMethod91()
+			{
+				System.out.println("running errorMethod");
+				throw new LinkageError();
+			}
+			public void errorMethod92()
+			{
+				System.out.println("running errorMethod6");
+				throw new ServiceConfigurationError(null);
+			}
+			public void errorMethod93()
+			{
+				System.out.println("running errorMethod7");
+				throw new ThreadDeath();
+			}
+			public void errorMethod94()
+			{
+				System.out.println("running errorMethod");
+				throw new TransformerFactoryConfigurationError();
+			}
+			public void errorMethod95()
+			{
+				System.out.println("running errorMethod95");
+				throw new InternalError();
+			}
+			public void errorMethod96()
+			{
+				System.out.println("running errorMethod96");
+				throw new OutOfMemoryError();
+			}
+			public void errorMethod97()
+			{
+				System.out.println("running errorMethod97");
+				throw new StackOverflowError();
+			}
+			public void errorMethod98()
+			{
+				System.out.println("running errorMethod");
+				throw new UnknownError();
+			}
+			public void errorMethod99()
+			{
+				System.out.println("running errorMethod99");
+				throw new AWTError(null);
+			}
+			public void errorMethod100()
+			{
+				System.out.println("running errorMethod100");
+				throw new TransformerFactoryConfigurationError();
+			}
+
 }
     
 
